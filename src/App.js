c924cdc2621cda7710a7e1f3cdd8b9a46a8e743d
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-03-02 09:26:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ import NavBar from './navbar.js';
 function App() {
   return (
     <div className="App">
-    <script src="./navbar.js"></script>
+    <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Features</a>
+      <a class="nav-item nav-link" href="#">Pricing</a>
+      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+    </div>
+  </div>
       <h1>"CS 230L"</h1>
       <h2>"Section 01</h2>
       <p>“WVU ID: 800358279”</p>
