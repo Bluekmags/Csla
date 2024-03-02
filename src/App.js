cd95fae1e2e94981e9e8212964c552ab497b186e
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-03-02 10:01:32 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,15 +23,18 @@ function App() {
       <h2>"Section 01</h2>
       <p>“WVU ID: 800358279”</p>
       <p>“Hi I am Kade McGlumphy”</p>
-    </div>
 
-  <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+<div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
   <div class="card-header">Header</div>
   <div class="card-body">
     <h5 class="card-title">Primary card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
 </div>
+    
+    </div>
+
+  
   );
 }
 
