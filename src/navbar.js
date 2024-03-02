17e2d41bbb2554be308bf92805b341c49e98a341
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-03-02 09:37:15 -0500

    Update navbar.js

--- src/navbar.js
+++ src/navbar.js
@@ -1,8 +1,1 @@
-function NavBar() {
-    return (
-    <h1>"CS 230L"</h1>
-      <h2>"Section 01</h2>
-      <p>“WVU ID: 800358279”</p>
-      <p>“Hi I am Kade McGlumphy”</p>
-    );
-  }
+<h1>Cool</h1>
