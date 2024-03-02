db9f78965f621661be0a1085cec69a73c637ad85
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-03-02 09:56:31 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
       <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
       <a class="nav-item nav-link" href="#">Features</a>
       <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
     </div>
   </div>
     </nav>
