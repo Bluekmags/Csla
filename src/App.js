d1498320b54814f4b04f386d65c5e21f84029516
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-03-03 14:35:37 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,32 +12,6 @@ function App() {
       <p>“WVU ID: 800358279”</p>
       <p>“Hi I am Kade McGlumphy”</p>
 
-      <div class="card-group">
-        <div class="card bg-primary text-white">
-          <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          </div>
-        </div>
-      
-        <div class="card bg-danger text-white">
-          <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          </div>
-        </div>
-      
-      
-        <div class="card bg-success text-white">
-          <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          </div>
-        
-      </div>
-    </div>
-  
-
     
     </div>
 
