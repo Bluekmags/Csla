b9569aebeed6485fcbd0f454908a1b917e16f8eb
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-03-03 14:22:42 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <script type="module">
          import NavBar from 'src/navbar.js';
+         
+      </script>
+         
       <h1>"CS 230L"</h1>
       <h2>"Section 01</h2>
       <p>“WVU ID: 800358279”</p>
