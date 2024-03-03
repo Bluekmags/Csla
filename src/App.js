5034067ccee4f94c658ff8fab6b165ce62faa7d9
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-03-03 15:52:36 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <NavBar/>
       
       <h1>"CS 230L"</h1>
-      <h2>"Section 01</h2>
+      <h2>"Section 01"</h2>
       <p>“WVU ID: 800358279”</p>
       <p>“Hi I am Kade McGlumphy”</p>
 
