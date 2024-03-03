1834e5381880e0798935278613517f7fa95d7cf9
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-03-02 21:00:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import NavBar from './navbar.js';
 function App() {
   return (
     <div className="App">
-      <script>
-        NavBar();
+      <script src="navbar.js">
       </script>
       <h1>"CS 230L"</h1>
       <h2>"Section 01</h2>
