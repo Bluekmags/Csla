9f9baa9efa5b55801a10e94d22013beaf9ce486d
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-03-03 15:43:55 -0500

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,7 @@
 function Cardwork() {
   return (
     <div class="row">
-      <div class="col-sm-6">
+      <div class="col-sm-4">
         <div class="card">
           <div class="card-body">
             This is some text within a card body.
