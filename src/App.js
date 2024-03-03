77360a814a89728ccb22a1c22dd24c1548f88e1b
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-03-03 15:11:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,13 +12,7 @@ function App() {
       <p>“WVU ID: 800358279”</p>
       <p>“Hi I am Kade McGlumphy”</p>
 
-      <div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">Special title treatment</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
+      
   
     
     </div>
