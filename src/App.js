3182b4c55a2a05ed560197df60bcb6068b9c7bec
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-03-03 14:40:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './navbar.js';
-import card from './Card.js';
+import cards from './Card.js';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <p>“WVU ID: 800358279”</p>
       <p>“Hi I am Kade McGlumphy”</p>
 
-      <card/>
+      <cards/>
     
     </div>
 
