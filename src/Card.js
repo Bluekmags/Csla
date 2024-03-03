87f195204c32c17b75c22498aae730b3cb1839e6
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-03-03 14:47:37 -0500

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,29 +1,12 @@
 function Card() {
   return (
-    
-      <div class="card-group">
-        <div class="card bg-primary text-white">
-          <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          </div>
-        </div>
-      
-        <div class="card bg-danger text-white">
-          <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          </div>
-        </div>
-      
-      
-        <div class="card bg-success text-white">
-          <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          </div>
-        </div>
+    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+      <div class="card-header">Header</div>
+      <div class="card-body">
+        <h5 class="card-title">Primary card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       </div>
+    </div>
     
   );
 }
