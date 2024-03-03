efde3097142cf827a24d641d966310198076c1d8
Bluekmags <156960982+Bluekmags@users.noreply.github.com>
2024-03-03 15:49:23 -0500

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -2,21 +2,21 @@ function Cardwork() {
   return (
     <div class="row">
       <div class="col-sm-4">
-        <div class="card">
+        <div class="card text-white bg-primary">
           <div class="card-body">
             This is some text within a card body.
           </div>
         </div>
       </div>
       <div class="col-sm-4">
-        <div class="card">
+        <div class="card text-white bg-danger">
           <div class="card-body">
             This is some text within a card body.
           </div>
         </div>
       </div>
       <div class="col-sm-4">
-        <div class="card">
+        <div class="card text-white bg-success">
           <div class="card-body">
             This is some text within a card body.
           </div>
